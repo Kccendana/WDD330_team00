33d0cd27275860275e687bff42d0778d50269215
kcccendana <kc_telyn@yahoo.com>
2025-03-22 08:02:08 +0900

    added event listener to sort the product

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -6,10 +6,10 @@ loadHeaderFooter();
 const category = getParam("category");
 const dataSource = new ProductData();
 const element = document.querySelector(".product-list");
-const title = document.querySelector(".product-categoryTitle");
-title.innerHTML = category.charAt(0).toUpperCase()
-+ category.slice(1)
 const list = new ProductList(category, dataSource, element);
 list.init();
 
 
+document.getElementById("sort-by").addEventListener("change", (event) => {
+    list.sortProducts(event.target.value);
+  });
