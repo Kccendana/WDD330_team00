246260cc3f0b1cb94ab510ee58fdfaaa4855fd12
sofiapantas <sofiasarabia0831@gmail.com>
2025-03-28 00:46:15 +0800

    input validation

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -1,11 +1,25 @@
 const baseURL = import.meta.env.VITE_SERVER_URL
 
-function convertToJson(res) {
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error("Bad Response");
+async function convertToJson(res) {
+  // if (res.ok) {
+  //   return res.json();
+  // } else {
+  //   throw new Error("Bad Response");
+  // }
+  try {
+    const jsonResponse = await res.json();
+    if (res.ok) {
+      return jsonResponse;
+    }
+    else {
+      throw {name: 'servicesError', message: jsonResponse};
+    }
   }
+  catch (error) {
+    throw {name: 'servicesError', message: error.message};
+
+  }
+
 }
 
 export default class ExternalServices {
