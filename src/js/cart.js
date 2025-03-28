d3057087621db424c6ee77a9e7ee2b130ccf8b31
Natnael31 <end24002byui.edu>
2025-03-28 18:05:36 +0300

    update

--- src/js/cart.js
+++ src/js/cart.js
@@ -8,6 +8,7 @@ import {
 } from "./utils.mjs";
 
 function renderCartContents() {
+  const cartItems = getLocalStorage("so-cart") || [];
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 
