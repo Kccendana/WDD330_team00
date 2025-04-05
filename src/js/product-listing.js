8bb781813a22099ca1afda20493e8effa238e35d
Oluwafunmilayo01 <oluwafunmilayome@gmail.comgit config --list>
2025-04-05 23:44:07 +0100

    product search

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -9,7 +9,27 @@ const element = document.querySelector(".product-list");
 const list = new ProductList(category, dataSource, element);
 list.init();
 
-
 document.getElementById("sort-by").addEventListener("change", (event) => {
-    list.sortProducts(event.target.value);
-  });
+  list.sortProducts(event.target.value);
+});
+
+// eslint-disable-next-line no-unused-vars
+const search = () => {
+  const searchbox = document.getElementById("search-item").value.toUpperCase();
+  const storeitems = document.getElementById("product-list");
+  const product = document.querySelectorAll("product");
+  const pname = storeitems.getElementsByTagName("h2");
+
+  for (var i = 0; i < pname.length; i++) {
+    let match = product[i].getElementsByTagName("h2")[0];
+
+    if (match) {
+      let textvalue = match.textContent || match.innerHTML;
+      if (textvalue.toUpperCase().indexOf(searchbox) > -1) {
+        product[1].computedStyleMap.display = "";
+      } else {
+        product[1].computedStyleMap.display = "none";
+      }
+    }
+  }
+};
